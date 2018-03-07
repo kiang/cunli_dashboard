67a95eee37f4ba81ef221dc58baa4ad54876a6a2
kiang <kiange@gmail.com>
2018-03-07 23:01:59 +0800

    rename using fixed key

--- scripts/02_rename.php
+++ scripts/02_rename.php
@@ -4,15 +4,17 @@ $fh = fopen(__DIR__ . '/villages.csv', 'r');
 fgetcsv($fh, 2048);
 $ref = array();
 while($line = fgetcsv($fh, 2048)) {
-  $line[1] = substr($line[1], 0, 7) . substr($line[1], 8);
-  $ref[$line[1]] = $line[2];
+  $parts = explode('-', $line[1]);
+  $ref[$line[4] . $line[3] . $parts[1]] = $line[2];
 }
 
 foreach(glob($path . '/cunli/*.json') AS $jsonFile) {
   $json = json_decode(file_get_contents($jsonFile), true);
   foreach($json['features'] AS $k => $f) {
-    if(isset($ref[$f['properties']['VILLAGE_ID']]) && ($f['properties']['V_Name'] !== $ref[$f['properties']['VILLAGE_ID']])) {
-      $json['features'][$k]['properties']['V_Name'] = $ref[$f['properties']['VILLAGE_ID']];
+    $parts = explode('-', $f['properties']['VILLAGE_ID']);
+    $key = $f['properties']['C_Name'] . $f['properties']['T_Name'] . $parts[1];
+    if(isset($ref[$key]) && ($f['properties']['V_Name'] !== $ref[$key])) {
+      $json['features'][$k]['properties']['V_Name'] = $ref[$key];
     }
   }
   file_put_contents($jsonFile, json_encode($json));
